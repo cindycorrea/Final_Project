682aee3c79c8e6f46ca872691b28eef4c1222720
cindy correa <cindycorrea04@gmail.com>
2024-03-11 11:06:18 -0600

    updated test function

--- controllers/fantasy.js
+++ controllers/fantasy.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const test = async (request, response) => {
     console.log("This is the test.")
-    response.json('You have arrived.')
+    response.json('This is the second test function.')
 }
 
 module.exports = { test }
