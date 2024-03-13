e4b77857e065feecd39648c3261507b3a4d1f81e
Stephanie <sventura102@gmail.com>
2024-03-13 11:12:29 -0700

    resolve merge conflict

--- schemas/mystery.js
+++ schemas/mystery.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 
-const mysterySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const mysterySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    published: {
+      type: Number,
+      required: true,
+    },
+    pgCount: {
+      type: Number,
+      required: true,
+    },
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    audience: {
+      type: String,
+      required: true,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  published: {
-    type: Number,
-    required: true,
-  },
-  pgCount: {
-    type: Number,
-    required: true,
-  },
-  synopsis: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-  audience: {
-    type: String,
-    required: true,
-  },
-}, { collection: 'mystery' });
+  { collection: "mystery" }
+);
 
 const Mystery = mongoose.model("Mystery", mysterySchema);
 
