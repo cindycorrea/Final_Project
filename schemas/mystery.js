1342f8f934be414fd301651a5f0c59ff06f6ec34
emrmason <mas20021@byui.edu>
2024-03-13 10:39:28 -0700

    Schemas specific for collections

--- schemas/mystery.js
+++ schemas/mystery.js
@@ -1,36 +1,39 @@
 const mongoose = require("mongoose");
 
-const mysterySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const mysterySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    published: {
+      type: Number,
+      required: true,
+    },
+    pgCount: {
+      type: Number,
+      required: true,
+    },
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    audience: {
+      type: String,
+      required: true,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  published: {
-    type: Number,
-    required: true,
-  },
-  pgCount: {
-    type: Number,
-    required: true,
-  },
-  synopsis: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-  audience: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: "mystery" }
+);
 
-const Mystery = mongoose.model("Fantasy", mysterySchema);
+const Mystery = mongoose.model("Mystery", mysterySchema);
 
 module.exports = Mystery;
--- schemas/romance.js
+++ schemas/romance.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 
-const romanceSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const romanceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    published: {
+      type: Number,
+      required: true,
+    },
+    pgCount: {
+      type: Number,
+      required: true,
+    },
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    audience: {
+      type: String,
+      required: true,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  published: {
-    type: Number,
-    required: true,
-  },
-  pgCount: {
-    type: Number,
-    required: true,
-  },
-  synopsis: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-  audience: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: "romance" }
+);
 
 const Romance = mongoose.model("Romance", romanceSchema);
 
