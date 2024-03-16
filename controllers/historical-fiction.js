1470d3b73d875b7f61dcd45a59070e33c0218b7e
emrmason <mas20021@byui.edu>
2024-03-16 13:56:15 -0700

    Historical Fiction get one by ID complete, working

--- controllers/historical-fiction.js
+++ controllers/historical-fiction.js
@@ -12,6 +12,18 @@ const listHF = async (req, res, next) => {
   }
 };
 
+const oneHF = async (req, res, next) => {
+  const hfId = req.params.id;
+  try {
+    const result = await HFiction.findById(hfId);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
 module.exports = {
   listHF,
+  oneHF,
 };
--- routes/historical-fiction.js
+++ routes/historical-fiction.js
@@ -3,7 +3,7 @@ const hfController = require("../controllers/historical-fiction");
 const { requiresAuth } = require("express-openid-connect");
 
 routes.get("/historical-fiction", hfController.listHF);
-// routes.get("/historical-fiction/:id", hfController.oneHF);
+routes.get("/historical-fiction/:id", hfController.oneHF);
 
 // routes.post("/historical-fiction", requiresAuth(), hfController.newHF);
 
