286c5ccf72077f78433731b551fb19e9c482ce27
Stephanie <sventura102@gmail.com>
2024-03-18 13:09:26 -0700

    added routes to controller function

--- routes/mystery.js
+++ routes/mystery.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const { requiresAuth } = require("express-openid-connect");
 const mysteryController = require("../controllers/mystery")
 
-router.get("/mystery", requiresAuth(), mysteryController.getAllMystery);
+router.get("/mystery", mysteryController.getAllMystery);
+
+router.get("/mystery/:id", mysteryController.getOneMystery);
+
+router.post("/mystery", requiresAuth(), mysteryController.createMystery);
+
+router.put("/mystery/:id", requiresAuth(), mysteryController.updateMystery);
+
+router.delete("/mystery/:id", requiresAuth(), mysteryController.deleteMystery);
 
 module.exports = router;
