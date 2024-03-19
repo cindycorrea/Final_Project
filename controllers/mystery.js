465b3acbb44f2a88fa529b6bd5b4fcf0ffea397b
Stephanie <sventura102@gmail.com>
2024-03-18 20:00:03 -0700

    finalized post and delete functions

--- controllers/mystery.js
+++ controllers/mystery.js
@@ -19,14 +19,38 @@ const getOneMystery = async(request, response) => {
 }
 
 const createMystery = async(request, response) => {
-
+  const book = new Mystery({title: request.body.title, author: request.body.author, published: request.body.published, pgCount: request.body.pgCount, synopsis: request.body.synopsis, genre: request.body.genre, audience: request.body.audience});
+  try { 
+    const result = await book.save();
+    response.status(201).json(`${result.title} has been added to the Mystery genre.`)
+  } catch (error) {
+    console.error("Error:", error);
+    response.status(500).json('Please try again.');
+  }
 }
 
 const updateMystery = async(request, response) => {
+  const id = request.params.id;
+  const book = Mystery({title: request.body.title, author: request.body.author, published: request.body.published, pgCount: request.body.pgCount, synopsis: request.body.synopsis, genre: request.body.genre, audience: request.body.audience});
+  try {
+    const result = await Mystery.findOneAndUpdate({_id: id}, book);
+    response.status(201).json(`${result.title} has been updated.`)
+  } catch (error) {
+    console.error("Error:", error);
+    response.status(500).json('Must be a valid id');
+  }
 
 }
 
-const deleteMystery = async(request, respone) => {
+const deleteMystery = async(request, response) => {
+  const id = request.params.id;
 
+  try {
+    const result = await Mystery.findByIdAndDelete(id);
+    response.status(201).json(`${result.title} has been deleted.`)
+  } catch (error){
+    console.error("Error:", error);
+    response.status(500).json('Must be a valid id');
+  }
 }
 module.exports = { getAllMystery, getOneMystery, createMystery, updateMystery, deleteMystery}
