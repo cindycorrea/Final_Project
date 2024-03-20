875eecd1dce89ee4856a791c73398be2477ff7bb
Stephanie <sventura102@gmail.com>
2024-03-20 10:21:48 -0700

    fixed update function

--- controllers/mystery.js
+++ controllers/mystery.js
@@ -3,54 +3,79 @@ const mongoDB = require("../connectDB/connection");
 const Mystery = require("../schemas/mystery");
 
 const getAllMystery = async (request, response) => {
-    const mystery = await Mystery.find();
-    response.json(mystery);
-  };
+  const mystery = await Mystery.find();
+  response.json(mystery);
+};
 
-const getOneMystery = async(request, response) => {
+const getOneMystery = async (request, response) => {
   const id = request.params.id;
   try {
-  const result = await Mystery.findById(id);
-  response.send(result);
+    const result = await Mystery.findById(id);
+    response.send(result);
   } catch (error) {
     console.error("Error:", error);
-    response.status(500).json('Must be a valid id');
+    response.status(500).json("Must be a valid id");
   }
-}
+};
 
-const createMystery = async(request, response) => {
-  const book = new Mystery({title: request.body.title, author: request.body.author, published: request.body.published, pgCount: request.body.pgCount, synopsis: request.body.synopsis, genre: request.body.genre, audience: request.body.audience});
-  try { 
+const createMystery = async (request, response) => {
+  const book = new Mystery({
+    title: request.body.title,
+    author: request.body.author,
+    published: request.body.published,
+    pgCount: request.body.pgCount,
+    synopsis: request.body.synopsis,
+    genre: request.body.genre,
+    audience: request.body.audience,
+  });
+  try {
     const result = await book.save();
-    response.status(201).json(`${result.title} has been added to the Mystery genre.`)
+    response
+      .status(201)
+      .json(`${result.title} has been added to the Mystery genre.`);
   } catch (error) {
     console.error("Error:", error);
-    response.status(500).json('Please try again.');
+    response.status(500).json("Please try again.");
   }
-}
-// Update not working as intended //
-const updateMystery = async(request, response) => {
+};
+
+const updateMystery = async (request, response) => {
   const id = request.params.id;
-  const book = Mystery({title: request.body.title, author: request.body.author, published: request.body.published, pgCount: request.body.pgCount, synopsis: request.body.synopsis, genre: request.body.genre, audience: request.body.audience});
+  const book = {
+    $set: {
+      title: request.body.title,
+      author: request.body.author,
+      published: request.body.published,
+      pgCount: request.body.pgCount,
+      synopsis: request.body.synopsis,
+      genre: request.body.genre,
+      audience: request.body.audience,
+    },
+  };
   try {
-    const result = await Mystery.findOneAndUpdate({_id: id}, book);
-    response.status(201).json(`${result.title} has been updated.`)
+    const result = await Mystery.findOneAndUpdate({ _id: id }, book);
+    response.status(204).send();
   } catch (error) {
     console.error("Error:", error);
-    response.status(500).json('Must be a valid id');
+    response.status(500).json("Must be a valid id");
   }
+};
 
-}
-
-const deleteMystery = async(request, response) => {
+const deleteMystery = async (request, response) => {
   const id = request.params.id;
 
   try {
     const result = await Mystery.findByIdAndDelete(id);
-    response.status(201).json(`${result.title} has been deleted.`)
-  } catch (error){
+    response.status(201).json(`${result.title} has been deleted.`);
+  } catch (error) {
     console.error("Error:", error);
-    response.status(500).json('Must be a valid id');
+    response.status(500).json("Must be a valid id");
   }
-}
-module.exports = { getAllMystery, getOneMystery, createMystery, updateMystery, deleteMystery}
+};
+module.exports = {
+  getAllMystery,
+  getOneMystery,
+  createMystery,
+  updateMystery,
+  deleteMystery,
+};
